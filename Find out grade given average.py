b51e483d5f352eff327bce5f242f713c68752005
SanthiPriyaa <pullagurasanthipriya2004@gmail.com>
2026-02-09 12:50:51 +0530

    Add files via upload

---
+++ Find out grade given average.py
@@ -0,0 +1,13 @@
+average = 82
+if 90 <= average <= 100:
+  print("A")
+elif 80 <= average < 89:
+  print("B")
+elif 70 <= average < 79:
+  print("C")
+elif 60 <= average < 69:
+  print("D")
+elif 0 <= average < 59:
+  print("F")
+else: 
+  print("Invalid average")
